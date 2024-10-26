c01902a63b24ba31bf8e7fec3839ddbf649a713f
Abel Simon Zachariah <abelsimonz@gmail.com>
2024-10-26 17:53:29 +0530

    Fixed colour

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -172,7 +172,7 @@ app.frame('/trans', (c) => {
 
   return c.res({
     image: (
-      <div style={{ color: 'white', display: 'flex', fontSize: 60 }}>
+      <div style={{ color: 'white', backgroundColor: 'black', display: 'flex', fontSize: 60 }}>
         Voting Contract
         XMTP Address ID: {address}
       </div>
